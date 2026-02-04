e3fde4e8331712b87a89de2c2f3260aa015f26a3
omaralfarra09-dev <omar.alfarra09@gmail.com>
2026-02-04 19:13:23 +0300

    testing css

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import autoprefixer from 'autoprefixer';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
@@ -13,10 +14,16 @@ export default defineConfig({
 			strategy: ['url', 'cookie', 'baseLocale']
 		})
 	],
+
+	css: {
+		postcss: {
+			plugins: [autoprefixer()]
+		}
+	},
 	build: {
 		// 1. Tell Vite to transpile JS for older browsers
 		target: 'es2015',
 		// 2. Tell Lightning CSS to downlevel modern CSS features
-		cssTarget: 'chrome80',
+		cssTarget: 'chrome80'
 	}
 });
