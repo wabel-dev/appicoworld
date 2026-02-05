d2ec355550154c4e642f5670577ca062d5af1961
omaralfarra09-dev <omar.alfarra09@gmail.com>
2026-02-05 21:46:55 +0300

    v3

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import TailwindLegacyPlugin from 'vite-plugin-tailwind-legacy';
+// import TailwindLegacyPlugin from 'vite-plugin-tailwind-legacy';
 
 export default defineConfig({
 	plugins: [
@@ -13,8 +13,8 @@ export default defineConfig({
 			outdir: './src/lib/paraglide',
 			strategy: ['url', 'cookie', 'baseLocale']
 		}),
-		TailwindLegacyPlugin({
-			tailwindConfig: 'tailwind.config.legacy.js'
-		})
+		// TailwindLegacyPlugin({
+		// 	tailwindConfig: 'tailwind.config.legacy.js'
+		// })
 	]
 });
